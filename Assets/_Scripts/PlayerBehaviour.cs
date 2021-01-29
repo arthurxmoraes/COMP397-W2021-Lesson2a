2fbac7f7acc4671722eae0244cc452dc26f237b2
Arthur Moraes <ivson.ma@gmail.com>
2021-01-29 18:10:36 -0500

    added basic input to player cube

---
+++ Assets/_Scripts/PlayerBehaviour.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+
+    public float movementForce;
+    public Rigidbody rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame - once every 16.6666ms
+    //1000ms per second
+    //approximately updates 60 times per second = 60fps
+    void Update()
+    {
+        if(Input.GetAxisRaw("Horizontal") > 0)
+        {
+            //move to right
+            //Debug.Log("Moving Right");
+            rigidBody.AddForce(Vector3.right * movementForce);
+        }
+
+        if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            //move to left
+            //Debug.Log("Moving Left");
+            rigidBody.AddForce(Vector3.left * movementForce);
+        }
+    }
+}
